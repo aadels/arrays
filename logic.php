a6b90bad618d1cac888c745ef91c41adc5bb2c7c
ann adelsberger <adelsbergerann@gmail.com>
2013-09-29 12:34:16 -0400

    fixed variable spelling error

--- logic.php
+++ logic.php
@@ -4,11 +4,10 @@ $contestant["julie"] = "Loser";
 $contestant["ann"] = "Winner";
 $contestant["larry"] = "Loser";
 $contestant["mairead"] = "Winner";
-
-<pre>
-	<?php print_r(?contestants); ?>
-</pre>
 ?>
 
 <h1>Contestants</h1>
 
+<pre>
+	<?php print_r(?contestant); ?>
+</pre>
