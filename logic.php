89cbc9cb083f30f036d024b58d2f713330f0d5da
ann adelsberger <adelsbergerann@gmail.com>
2013-09-29 12:09:55 -0400

    add logic.php

---
+++ logic.php
@@ -0,0 +1,9 @@
+<?php
+$contestant["rick"] = "Winner";
+$contestant["julie"] = "Loser";
+$contestant["ann"] = "Winner";
+$contestant["larry"] = "Loser";
+$contestant["mairead"] = "Winner";
+?>
+
+Larry is a <?=$contestant['larry']?>;
